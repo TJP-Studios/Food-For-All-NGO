4291bd06af833be05791dc555b50f18cec8d3dcb
Obyno003 <ogbonnaemmanuel003@gmail.com>
2023-11-09 03:05:51 +0100

    Added some functionalities to the donation page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Header & Footer
 const hamburgerIcon = document.querySelector ('.hamburger-icon')
 const closeIcon = document.querySelector ('.close-icon')
 const hamburgerMenu = document.querySelector ('.hamburger-menu')
@@ -15,3 +16,69 @@ closeIcon.addEventListener('click', ()=> {
     hamburgerMenu.classList.toggle('visible');
 }
 )
+
+// Donation Page
+const dedicatedCheckbox = document.querySelector ('.dedicated-checkbox')
+const dedicatedInput = document.querySelector ('.dedicated-input')
+
+dedicatedCheckbox.addEventListener('click', ()=> {
+    dedicatedInput.classList.toggle('visible');
+})
+
+
+const cardPaymentInactive = document.querySelector ('.card-inactive')
+const bankPaymentActive = document.querySelector ('.bank-active')
+const ussdPaymentActive = document.querySelector ('.ussd-active')
+const paypalPaymentActive = document.querySelector ('.paypal-active')
+const cardContainer = document.querySelector ('.card-payment-container')
+const bankContainer = document.querySelector ('.bank-transfer-payment-container')
+const ussdContainer = document.querySelector ('.ussd-payment-container')
+const paypalContainer = document.querySelector ('.paypal-payment-container')
+
+bankPaymentActive.addEventListener('click', ()=> {
+    cardPaymentInactive.classList.add('inactive');
+    bankPaymentActive.classList.add('active');
+    ussdPaymentActive.classList.remove('active');
+    paypalPaymentActive.classList.remove('active');
+
+    cardContainer.classList.add('hide')
+    bankContainer.classList.add('hide')
+    ussdContainer.classList.remove('hide')
+    paypalContainer.classList.remove('hide')
+})
+
+ussdPaymentActive.addEventListener('click', ()=> {
+    cardPaymentInactive.classList.add('inactive');
+    bankPaymentActive.classList.remove('active');
+    ussdPaymentActive.classList.add('active');
+    paypalPaymentActive.classList.remove('active');
+
+    cardContainer.classList.add('hide')
+    bankContainer.classList.remove('hide')
+    ussdContainer.classList.add('hide')
+    paypalContainer.classList.remove('hide')
+})
+
+paypalPaymentActive.addEventListener('click', ()=> {
+    cardPaymentInactive.classList.add('inactive');
+    bankPaymentActive.classList.remove('active');
+    ussdPaymentActive.classList.remove('active');
+    paypalPaymentActive.classList.add('active');
+
+    cardContainer.classList.add('hide')
+    bankContainer.classList.remove('hide')
+    ussdContainer.classList.remove('hide')
+    paypalContainer.classList.add('hide')
+})
+
+cardPaymentInactive.addEventListener('click', ()=> {
+    cardPaymentInactive.classList.add('inactive');
+    bankPaymentActive.classList.remove('active');
+    ussdPaymentActive.classList.remove('active');
+    paypalPaymentActive.classList.remove('active');
+
+    cardContainer.classList.remove('hide')
+    bankContainer.classList.remove('hide')
+    ussdContainer.classList.remove('hide')
+    paypalContainer.classList.remove('hide')
+})
